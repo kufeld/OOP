221097e4f7fed6d36cc43125f56d9a86f1f60934
A.Kufeld <kufeldnp@gmail.com>
2020-10-16 12:03:14 +0500

    Выполнение домашнего задания №1. Добавление фильтра без перевода в HSL

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/GammaCorrectionParameters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    class GammaCorrectionParameters : IParameters
+    {
+        public double GammaParameter { get; set; }
+        public ParameterInfo[] GetDescription()
+        {
+            return new[]
+            {
+                new ParameterInfo() {
+                    Name = "Гамма",
+                    MinValue = 0.2,
+                    MaxValue = 10,
+                    DefailtValue = 1,
+                    Increment = 0.01
+                    }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            GammaParameter = values[0];
+        }
+    }
+}
