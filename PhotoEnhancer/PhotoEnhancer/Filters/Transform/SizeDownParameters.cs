0f55af586347008f798dad497369fb6cea8885a8
A.Kufeld <kufeldnp@gmail.com>
2020-12-09 21:36:52 +0500

    Задание 2. Фильтры с использованием дженериков и делегатов. Куфельд Анастасия УГИ-296004 3 вариант

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/Transform/SizeDownParameters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class SizeDownParameters : IParameters
+    {
+        public double SizeDownParameter { get; set; }
+
+        public ParameterInfo[] GetDescription()
+        {
+            return new[]
+            {
+                new ParameterInfo() {
+                    Name = "Коэффициент уменьшения",
+                    MinValue = 1,
+                    MaxValue = 10,
+                    DefailtValue = 1,
+                    Increment = 0.5
+                    }
+            };
+        }
+        public void SetValues(double[] values)
+        {
+            SizeDownParameter = values[0];
+        }
+    }
+}
---
+++ PhotoEnhancer/PhotoEnhancer/Filters/Transform/SizeDownTransformer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+
+namespace PhotoEnhancer
+{
+    public class SizeDownTransformer : ITransformer<SizeDownParameters>
+    {
+        public Size ResultSize { get; private set; }
+
+        Size originalSize;
+        double TransformParameter;
+
+        public void Initialize(Size size, SizeDownParameters parameters)
+        {
+            originalSize = size;
+            TransformParameter = parameters.SizeDownParameter;
+
+            ResultSize = new Size(
+                (int)(size.Width / TransformParameter),
+                (int)(size.Height / TransformParameter));
+
+            if (ResultSize.Width == 0 || ResultSize.Height == 0)
+            { ResultSize = size;
+                TransformParameter = 1;
+            }
+        }
+       
+        public Point? MapPoint(Point point)
+        {
+
+            var x = (int)(point.X * TransformParameter);
+            var y = (int)(point.Y * TransformParameter);
+
+            return new Point(x, y);
+        }
+    }
+}
