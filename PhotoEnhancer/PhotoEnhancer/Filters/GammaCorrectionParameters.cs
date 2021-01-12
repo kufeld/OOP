5acb81924bd573cbbd82634aa3bed722603ef88c
A.Kufeld <kufeldnp@gmail.com>
2021-01-12 15:49:25 +0500

    Куфельд Анастасия УГИ-296004 Задание 4

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/GammaCorrectionParameters.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PhotoEnhancer
+{
+    public class GammaCorrectionParameters : IParameters
+    {
+        [ParameterInfo(Name = "Гамма", MinValue = 0.2, MaxValue = 10, DefailtValue = 1, Increment = 0.01)]
+        public double GammaParameter { get; set; }
+    }
+}
---
+++ PhotoEnhancer/PhotoEnhancer/Filters/Transform/SizeDownParameters.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PhotoEnhancer
+{
+    public class SizeDownParameters : IParameters
+    {
+        [ParameterInfo(Name = "Коэффициент уменьшения", MinValue = 1, MaxValue = 10, DefailtValue = 1, Increment = 0.5)]
+        public double SizeDownParameter { get; set; }
+    }
+}
